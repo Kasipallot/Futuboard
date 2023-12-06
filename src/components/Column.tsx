263ee1764f90ebf139f0e6002e8707f72dceb614
aarne <aarne.aaltonen@aalto.fi>
2023-12-06 14:14:57 +0200

    Added simple task and task creation form component

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,6 +1,76 @@
-import { Typography } from "@mui/material"
-import { Column } from "../types"
+import { Box, Dialog, DialogContent, IconButton, Typography } from "@mui/material"
+import { Column, Task as TaskType } from "../types"
 import Paper from '@mui/material/Paper'
+import Task from "./Task"
+import AddIcon from '@mui/icons-material/Add';
+import {useState} from 'react'
+import TaskCreationForm from "./TaskCreationForm";
+
+interface FormData {
+  taskTitle: string,
+  sizeEstimate?: number,
+  corner1?: string,
+  corner2?: string,
+  description?: string,
+}
+
+const CreateTaskButton:React.FC = () => {
+  const [open, setOpen] = useState(false)
+
+    const handleOpenDialog = () => {
+      setOpen(true)
+  }
+  const handleCloseDialog = () => {
+      setOpen(false)
+  }
+  const handleSubmit = (data: FormData) => {
+    //data holds the task object. send to server. log for now
+    console.log(data)
+    setOpen(false)
+
+  }
+  return(
+    <Box>
+      <IconButton color="primary" aria-label="add task" onClick = {handleOpenDialog}>
+            <AddIcon />
+      </IconButton>
+      <Dialog open={open} onClose={handleCloseDialog}>
+              <DialogContent>
+                  <TaskCreationForm onSubmit = {handleSubmit} onCancel = {handleCloseDialog}/>
+              </DialogContent>
+        </Dialog>
+    </Box>
+    
+  )
+}
+
+
+interface TaskListProps {
+  column : Column
+}
+
+
+const TaskList: React.FC<TaskListProps> = ({ column }) => {
+
+  const tasks = column.tasks
+  return (
+    <div>
+      {tasks && tasks.length ? (
+        tasks.map((task: TaskType, index) => (
+          <Task
+            key={task.id}
+            task={task}
+            index = {index}
+          />
+        ))
+      ) : (
+        <Typography><b>"no tasks yet"</b></Typography>
+      )}
+    </div>
+  )
+}
+
+
 
 interface ColumnProps {
   column : Column,
@@ -8,12 +78,17 @@ interface ColumnProps {
 }
 
 const Column: React.FC<ColumnProps> = ({ column }) => {
+
+
   console.log(column);
   return (
     <>
       <Paper elevation={4} style={{ margin: '25px', width: '250px', height: '1000px', backgroundColor: '#E5DBD9' }}>
         <Typography  variant={'h5'}  gutterBottom>{column.title}</Typography>
+        <CreateTaskButton />
+        <TaskList column={column} />
       </Paper>
+
     </>
   );
 };
