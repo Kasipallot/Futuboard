67971f55afcd6b28eb8fc7c13e273ed05c8d9891
Samuli Öhman <samuli.ohman00@gmail.com>
2023-12-14 21:57:53 +0200

    fix: Backend routers now return rest_framework Response instead of HttpResponse.

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
+from quickstart import views as admin_views
 from futuboard import views
 from django.contrib import admin
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r'users', admin_views.UserViewSet)
+router.register(r'groups', admin_views.GroupViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
