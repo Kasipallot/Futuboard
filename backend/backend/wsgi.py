937d2c0eb48cc85041a92e468bc259a8df611808
matias <matias.tarmo@aalto.fi>
2024-01-15 16:36:52 +0200

    Backend for deployment

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
