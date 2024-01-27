8138d798d11eca3b79d7ac82647c38488000a701
Samuli Öhman <samuli.ohman00@gmail.com>
2024-01-27 17:02:12 +0200

    Feat: Added websocket to the django backend.

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -10,7 +10,15 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from futuboard.consumers import BoardConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter([
+        path('board/<uuid:board_id>', BoardConsumer.as_asgi())
+    ])
+})
