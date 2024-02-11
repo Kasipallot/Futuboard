825f33dcaf887a987144cb9aeed9fced11952bb4
Edrishakimi1 <edrishakimi00@gamil.com>
2024-02-11 20:30:01 +0800

    new test

--- cypress/e2e/BoardCreationTest.cy.tsx
+++ cypress/e2e/BoardCreationTest.cy.tsx
@@ -1,7 +1,7 @@
 describe('Futuboard Home Page Test', () => {
     it('should have the correct title', () => {
       // Visit the URL
-      cy.visit('http://localhost:5173/');
+      cy.visit('https://white-ocean-04e4e8003.4.azurestaticapps.net');
   
       // Assert that the title is what you expect
      cy.get('.MuiTypography-root').should('contain', 'Futuboard home page');
@@ -12,7 +12,7 @@ describe('Futuboard Home Page Test', () => {
   describe('Board Creation Test', () => {
     it('should open the dialog, allow entry, and submit the form', () => {
       // Replace with the correct URL
-      cy.visit('http://localhost:5173/');
+      cy.visit('https://white-ocean-04e4e8003.4.azurestaticapps.net');
   
       // Click the "Create board" button
       cy.contains('button', 'Create board').click();
@@ -20,19 +20,12 @@ describe('Futuboard Home Page Test', () => {
       cy.get('.MuiDialog-root').should('be.visible');
       // Find the input field by its label and type a name
       cy.get('.MuiDialog-root').find('label').contains('Name').parent().find('input').type('New Board Name');
+      cy.get('.MuiDialog-root').find('label').contains('Password').parent().find('input').type('123');
+
       // Click the "Submit" button within the form
       cy.get('.MuiDialog-root').contains('button', 'Submit').click();
-      cy.get('.MuiTypography-root').should('contain', 'New Board Name');
-
-
-    });
-  })
-
-  describe('Board Table Test', () => {
-    it('Adding tasks', () => {
-      // Replae with the correct URL
-      cy.visit('http://localhost:5173/board/1');
-      cy.get('.MuiTypography-root').should('contain', 'Backlog');
+      cy.get('.MuiTypography-root').should('contain', 'Enter Board Password');
+      cy.get('.MuiDialog-root').find('label').contains('Password').parent().find('input').type('123');
 
     });
   })
