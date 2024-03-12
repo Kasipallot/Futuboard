be7d4fcab779327fa7b8544c383cfb3d413aed26
Edrishakimi1 <edrishakimi00@gamil.com>
2024-03-12 14:09:32 +0800

    uusi versio

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  devDependencies: {
+    "@types/jest": "^29.5.12",
+    cypress: "^10.0.0",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,37 @@
+describe('Futuboard Home Page Test', () => {
+  it('should have the correct title', () => {
+    // Visit the URL
+    cy.visit('https://ashy-sea-0c7c52603.4.azurestaticapps.net');
+
+    // Assert that the title is what you expect
+   cy.get('.MuiTypography-root').should('contain', 'FutuBoard');
+
+  })
+});
+
+describe('Board Creation Test', () => {
+  it('should open the dialog, allow entry, and submit the form', () => {
+    // Replace with the correct URL
+    cy.visit('https://ashy-sea-0c7c52603.4.azurestaticapps.net');
+
+    // Click the "Create board" button
+    cy.get('button').contains('Create board').click();
+
+    cy.get('.MuiDialog-root').should('be.visible');
+    // Find the input field by its label and type a name
+    cy.get('.MuiDialog-root').find('label').contains('Name').parent().find('input').type('New Board Name');
+    cy.get('.MuiDialog-root').find('label').contains('Password').parent().find('input').type('123');
+
+    // Click the "Submit" button within the form
+    cy.get('.MuiDialog-root').contains('button', 'Submit').click();
+
+    //log in part2
+    cy.get('.MuiTypography-root').should('contain', 'Enter Board Password');
+    cy.get('.MuiDialog-root').find('label').contains('Password').parent().find('input').type('123');
+    cy.get('.MuiIconButton-root').click();
+    
+
+  });
+})
+
+
