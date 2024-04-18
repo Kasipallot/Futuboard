0be1725283bf0e6cad55816a7db4547e57b7443c
Antonio <antonio.skondras@gmail.com>
2024-04-18 11:41:05 +0300

    feat: created a modal viewer for displaying our CV

---
+++ frontend/src/components/home/ModalFrameCv.tsx
@@ -0,0 +1,82 @@
+import { useState, CSSProperties } from "react";
+import GroupsIcon from '@mui/icons-material/Groups';
+import CloseIcon from '@mui/icons-material/Close';
+
+const ModalFrameCv = () => {
+    const [show, setShow] = useState(false);
+    const display = show ? 'block' : 'none';
+    const inlineStyles: Record<string, CSSProperties> = {
+        modalViewer: {
+            display: display,
+            position: 'fixed',
+            zIndex: 1000,
+            paddingTop: '100px',
+            paddingBottom: '100px',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+            overflow: 'auto',
+            backgroundColor: 'white',
+        },
+        CVImage: {
+            position: 'absolute',
+            top: '40%',
+            left: '50%',
+            maxWidth: '80%',
+            maxHeight: '80%',
+            transform: 'translate(-50%, -50%) scale(0.9)',
+            animation: 'zoom 0.6s', // Using animation shorthand property
+        },
+    };
+    return (
+        <>
+            <style>
+                {`
+                    @keyframes zoom {
+                        from {
+                            opacity: 0;
+                            transform: translate(-50%, -50%) scale(0.7);
+                        }
+                        to {
+                            opacity: 1;
+                            transform: translate(-50%, -50%) scale(0.9);
+                        }
+                    }
+                `}
+            </style>
+            <GroupsIcon
+                onClick={() => setShow(true)}
+                sx={{
+                    color: 'black',
+                    cursor: 'pointer',
+                    padding: 0,
+                    '&:hover': {
+                        color: '#646cff',
+                    },
+                }}
+            />
+            <div style={inlineStyles.modalViewer} id="modal-viewer">
+                <CloseIcon
+                    onClick={() => setShow(false)}
+                    style={{
+                    fontSize: 40
+                    }}
+                    sx={{
+                        position: 'absolute',
+                        top: '15px',
+                        right: '35px',
+                        color: 'black',
+                        transition: '0.2s',
+                        cursor: 'pointer',
+                        '&:hover': {
+                            color: '#646cff', // Change color on hover
+                        },
+                    }}
+                />
+                <img style={inlineStyles.CVImage} id='CV-image' alt='a picture of our cv' src='../../images/Team_Kasipallot_CV.png' />
+            </div>
+        </>
+    );
+};
+export default ModalFrameCv;
